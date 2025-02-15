68236b7eac6065cd16fa1695cf423f958af5b9a0
salian97 <salian@ualberta.ca>
2025-02-15 14:59:51 -0700

    changes to index

--- clothing_app/app/(tabs)/index.tsx
+++ clothing_app/app/(tabs)/index.tsx
@@ -1,8 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 const app = () => {
   return (
-    <View>
-      <Text>clothing</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>clothing</Text>
     </View>
   )
 }
+export default app
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        },
+    text: {
+        color: 'white',
+        fontSize: 42,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        }
+    })
